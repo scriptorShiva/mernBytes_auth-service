492edbf27f598b030fe17e31f3c673558d11a467
Shiva Pal <shivapal108941@gmail.com>
2023-11-03 18:26:22 +0530

    [Add]: httpError for global error Handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import logger from './config/logger';
 const app = express();
 
 // Middleware
@@ -9,6 +10,23 @@ app.use(express.json()); // Parse JSON request bodies
 // Define a route
 app.get('/', (req, res) => {
     res.send('Welcome to auth service');
+});
+
+// Global Error Handler - This should be the last middleware in the chain
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || err.status || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
