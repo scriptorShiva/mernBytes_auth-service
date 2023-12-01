084d0733cfe523a64950623db8ac22dd86517103
Shiva Pal <shivapal108941@gmail.com>
2023-12-01 11:08:06 +0530

    Database configure with typeORM

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,19 @@
-import * as dotenv from 'dotenv';
-dotenv.config(); // This loads the variables from the .env file into process.env
+import { config } from 'dotenv';
+import path from 'path';
+
+// Load environment variables from .env file
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+
+// Extract necessary configurations
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
 export const Config = {
-    PORT: process.env.PORT || 5000, // Default to 5000 if PORT is not defined in the .env file
-    NODE_ENV: process.env.NODE_ENV || 'development', // Default to 'development' if NODE_ENV is not defined
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 };
