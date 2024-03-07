9fb81e7fe17b0015cfa9cc58547a4191f87eec63
Shiva Pal <shivapal108941@gmail.com>
2024-03-07 23:19:04 +0530

    Write tests for create tenant : Is User is authenticated? add authenticate middleware

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from '../config/data-source';
 import { Tenant } from '../entity/Tenant';
 import { TenantService } from '../services/TenantService';
 import logger from '../config/logger';
+import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
 
@@ -14,8 +15,10 @@ const tenantService = new TenantService(tenantRepository);
 // create instance of class
 const tenantController = new TenantController(tenantService, logger);
 
+//after adding "authenticate" this route only accessible when there it verifies valid token in cookie.
 router.post(
     '/',
+    authenticate,
     (req: CreateTenantRequest, res: Response, next: NextFunction) =>
         tenantController.create(req, res, next),
 );
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -3,17 +3,40 @@ import { AppDataSource } from '../../src/config/data-source';
 import request from 'supertest';
 import app from '../../src/app';
 import { Tenant } from '../../src/entity/Tenant';
+import createJWKSMock from 'mock-jwks';
+import { Roles } from '../../src/constants';
+
+// how we can get token in test : by using mock jwks server
 
 describe('POST /tenants', () => {
     let connection: DataSource;
+    // 1.
+    let jwks: ReturnType<typeof createJWKSMock>;
+    // 5.
+    let adminToken: string;
 
     beforeAll(async () => {
         connection = await AppDataSource.initialize();
+        //2 .
+        jwks = createJWKSMock('http://localhost:5501');
     });
 
     beforeEach(async () => {
         await connection.dropDatabase();
         await connection.synchronize();
+        // 3.
+        jwks.start();
+
+        //6.
+        adminToken = jwks.token({
+            sub: '1',
+            role: Roles.ADMIN,
+        });
+    });
+
+    // 4.
+    afterEach(() => {
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -28,8 +51,10 @@ describe('POST /tenants', () => {
                 address: 'Tenant address',
             };
 
+            //7.
             const response = await request(app)
                 .post('/tenants')
+                .set('Cookie', [`accessToken=${adminToken}`])
                 .send(tenantData);
 
             // Assert
@@ -42,13 +67,34 @@ describe('POST /tenants', () => {
                 address: 'Tenant address',
             };
 
-            await request(app).post('/tenants').send(tenantData);
+            // 8.
+            await request(app)
+                .post('/tenants')
+                .set('Cookie', [`accessToken=${adminToken}`])
+                .send(tenantData);
 
             const tenantRepository = connection.getRepository(Tenant);
             const tenants = await tenantRepository.find();
             expect(tenants).toHaveLength(1);
             expect(tenants[0].name).toBe(tenantData.name);
             expect(tenants[0].address).toBe(tenantData.address);
+        });
+
+        it('should return 401 if user is not autheticated', async () => {
+            const tenantData = {
+                name: 'Tenant name',
+                address: 'Tenant address',
+            };
+
+            const response = await request(app)
+                .post('/tenants')
+                .send(tenantData);
+            expect(response.statusCode).toBe(401);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(0);
         });
     });
 });
